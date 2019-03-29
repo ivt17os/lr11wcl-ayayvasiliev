f974f784aca1be402e2a221cfc860ef34d4ab50c
NEFUstudents <study.svfu@gmail.com>
2019-03-29 12:35:24 +0900

    Task 4

--- wc-l/wc-l.cpp
+++ wc-l/wc-l.cpp
@@ -11,7 +11,7 @@ using namespace std;
 
 int main(int argc, char** argv ) {
     long long t1, t2, freq;
-	string str;
+	char str[4096];
 	
 	//if (argc == 1) 
     //{
@@ -21,21 +21,24 @@ int main(int argc, char** argv ) {
 
 	cout << "Opening file a.txt\n"; // << argv[1] << "\n";
 	
-	ifstream f("a.txt");
+	FILE* f = fopen("a.txt", "rb");
 	
     QueryPerformanceFrequency((LARGE_INTEGER *)&freq);// запрашиваем число тиков в 1 сек
 
 
 	QueryPerformanceCounter((LARGE_INTEGER *)&t1);// смотрим время после окончания цикла
 	
-    int numOfLines = 0;
-    do {
-        getline(f, str);
-        numOfLines++;
-    } while (!f.eof());
+	long long nread, numOfLines = 0;
+    while (!feof(f)){
+		nread = fread(str, 1, 4096, f);
+		for (long long i = 0; i < nread;i++) {
+			if (str[i] == '\n') numOfLines++;
+		}
+	};
     
     QueryPerformanceCounter((LARGE_INTEGER *)&t2);// смотрим время после окончания цикла
 
+	fclose(f);
 
     cout.precision(3);
     cout << "Time: " << fixed << (t2-t1)/(1.*freq) << "sec" << endl;
